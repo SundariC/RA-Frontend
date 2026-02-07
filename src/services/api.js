d94d67ebd16dcea5a9a23288f526001041a80dbd
Sundari <csundari2000@gmail.com>
2026-02-07 12:35:05 +0530

    render link added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", 
+  baseURL: "https://ra-backend-61n5.onrender.com/api", 
 });
 
 API.interceptors.request.use((req) => {
